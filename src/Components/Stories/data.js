24f5df67a66e008a8dabf04d4af2194526b9b09d
Oliver Plunkett <otplunkett@gmail.com>
2019-02-26 14:31:00 -0500

    fix dates

--- src/Components/Stories/data.js
+++ src/Components/Stories/data.js
@@ -5,36 +5,36 @@ export const articles = [
     url: "https://www.lustre.net/home/2019/1/14/miranda-on-travel-safari-basics"
   },
   {
+    title: "Quebec - The Other Road Trip",
+    date: "October, 2018",
+    url:
+      "https://www.lustre.net/home/2018/8/20/miranda-on-travel-canadian-road-trips"
+  },
+  {
     title: "An Inside Look at Latin America",
     date: "September, 2018",
     url: "https://www.lustre.net/home/2018/9/6/zrwvw0q7zasrs1klweh2pe817qfxth"
   },
   {
-    title: "Quebec - The Other Road Trip",
-    date: "August, 2018",
-    url:
-      "https://www.lustre.net/home/2018/8/20/miranda-on-travel-canadian-road-trips"
-  },
-  {
     title: "Not Only For Grandchildren",
-    date: "July, 2018",
+    date: "August, 2018",
     url:
       "https://www.lustre.net/home/2018/7/10/not-only-for-grandchildren-by-miranda-plunkett"
   },
   {
     title: "What Brexit?",
-    date: "June, 2018",
+    date: "July, 2018",
     url:
       "https://www.lustre.net/home/2018/6/14/miranda-plunkett-writes-what-brexit"
   },
   {
     title: "Advice on Navigating the Travel Industry",
-    date: "March, 2018",
+    date: "May, 2018",
     url: "https://www.lustre.net/home/2018/3/25/u7jjsfwy0i1m1n43oa4155fo4epu4z"
   },
   {
     title: "An Expert on The Other Wine Country",
-    date: "March, 2018",
+    date: "April, 2018",
     url:
       "https://www.lustre.net/home/2018/3/25/viewpoints-miranda-a-travel-expert-on-the-other-wine-country-3Dvqq"
   }
